d7032f525ed900d8d41912628ace1f8621785686
virendraravalji <virendraravalji@qnap.com>
2019-07-01 21:29:24 +0800

    Create a project repoistory

---
+++ MyShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyShop
+//
+//  Created by Virendra Ravalji on 2019/7/1.
+//  Copyright © 2019 Virendra Ravalji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
